0ff4436d162c3ee989587cbf03871e91c95d1cb3
Benjamin <benjaxdxd04@gmail.com>
2026-06-09 16:49:13 -0400

    agrego imagen perros

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,12 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div style={{ display: "flex", justifyContent: "center", alignItems: "center", minHeight: "100vh" }}>
+    <div style={{ display: "flex", flexDirection: "column", justifyContent: "center", alignItems: "center", minHeight: "100vh", gap: "2rem" }}>
       <h1 style={{ fontSize: "2rem", fontFamily: "sans-serif" }}>
         ola mamita estoy avanzando en la pagina
       </h1>
+      <Image src="/perros.png" alt="perros" width={400} height={500} />
     </div>
   );
 }
